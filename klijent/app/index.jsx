edf199d54935859384bd1fe96bdeaf972e1fb795
Josip Sikiric <josip.sikiric@proton.me>
2025-04-15 18:48:54 +0200

    asinkroni kod za speedtest

--- klijent/app/index.jsx
+++ klijent/app/index.jsx
@@ -7,16 +7,20 @@ export default function Index() {
   const [data, setData] = useState(null);
   const [error, setError] = useState('');
 
-  useEffect(async () => {
-    try {
-      const response = await fetch('http://localhost:5000/api/speedtest');
-      const json = await response.json();
-      setData(json);
-    } catch (e) {
-      setError(e);
-    }
-  }, [])
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/speedtest');
+        const json = await response.json();
+        setData(json);
+      } catch (e) {
+        setError(e.message || 'Unknown error');
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
 
   return (
     <View
@@ -28,11 +32,7 @@ export default function Index() {
     >
       <View>
         {error ? error.toString() : (data ? 
-        <>
-          <Text>Download speed: {data['download']}</Text>
-          <br />
-          <Text>Upload speed: {data['upload']}</Text>
-        </>
+        <Text>Download speed: {data.download}{"\n"}Upload speed: {data.upload}</Text>
          : <ActivityIndicator size="large" />)}
       </View>
     </View>
--- server/main.py
+++ server/main.py
@@ -1,9 +1,28 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from testspeed import test_speed
+from threading import Thread
+import time
 
 app = Flask(__name__)
 CORS(app)
+
+
+latest_speed = {'download': 0, 'upload': 0}
+
+
+def update_speed():
+  global latest_speed
+  while True:
+    down_speed, up_speed = test_speed()
+    latest_speed = {
+      'download': str(down_speed),
+      'upload': str(up_speed)
+    }
+    time.sleep(600)
+
+
+
 
 
 ################# ROUTING #################
@@ -12,15 +31,15 @@ CORS(app)
 def get_data():
   return jsonify({'message': 'Hello from Flask!'})
 
-@app.route('/api/speedtest', methods=['GET', 'POST'])
+@app.route('/api/speedtest', methods=['GET'])
 def get_speed():
-  down_speed = 0
-  up_speed = 0
-  
-  if request.method == 'GET':
-    (down_speed, up_speed) = test_speed()
-  
-  return jsonify({
-    'download': str(down_speed),
-    'upload': str(up_speed)
-  })
+  return jsonify(latest_speed)
+
+
+
+
+
+if __name__ == '__main__':
+    print("[Flask] Starting server...")
+    Thread(target=update_speed, daemon=True).start()
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
