3af658090b02ea352d85fdca16e19631f50e9a05
Josip Sikiric <josip.sikiric@proton.me>
2025-04-29 19:03:20 +0200

    no bloat

--- server/app.py
+++ server/app.py
@@ -1,11 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from threading import Thread
-import time
 from dotenv import load_dotenv
 import os
-# from testspeed import test_speed
-# from scrape import scrape_books
 
 app = Flask(__name__)
 CORS(app)
@@ -14,37 +10,12 @@ load_dotenv()
 app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
 
 
-# latest_speed = {'download': 0, 'upload': 0}
-
-
-# def update_speed():
-#   global latest_speed
-#   while True:
-#     down_speed, up_speed = test_speed()
-#     latest_speed = {
-#       'download': str(down_speed),
-#       'upload': str(up_speed)
-#     }
-#     time.sleep(600)
-
-
-
-
 
 ################# ROUTING #################
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-  return jsonify({'message': 'Hello from Flask!'})
-
-# @app.route('/api/speedtest')
-# def get_speed():
-#   return jsonify(latest_speed)
-
-# @app.route('/api/books')
-# def get_books():
-#   return jsonify(scrape_books())
-
+# @app.route('/api/data', methods=['GET'])
+# def get_data():
+#   return jsonify({'message': 'Hello from Flask!'})
 
 
 
